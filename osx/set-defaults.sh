22f7445163076eeb49889cc00dd662e61708e773
Matt McManus <mattmcmanus@gmail.com>
2013-03-09 18:49:30 -0500

    Follow mouse is doing something wierd

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -210,8 +210,8 @@ defaults write com.apple.terminal StringEncodings -array 4
 
 # Enable “focus follows mouse” for Terminal.app and all X11 apps
 # This means you can hover over a window and start typing in it without clicking first
-defaults write com.apple.terminal FocusFollowsMouse -bool true
-defaults write org.x.X11 wm_ffm -bool true
+#defaults write com.apple.terminal FocusFollowsMouse -bool true
+#defaults write org.x.X11 wm_ffm -bool true
 
 # Disable the Ping sidebar in iTunes
 defaults write com.apple.iTunes disablePingSidebar -bool true
