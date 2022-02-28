8aa8d10d34402fb1182dfddb7b2265eade4504d7
Matt McManus <matt@mcmanus.io>
2022-02-28 10:17:02 -0500

    Updates

--- Brewfile
+++ Brewfile
@@ -1,4 +1,6 @@
+tap "dart-lang/dart"
 tap "federico-terzi/espanso"
+tap "hashicorp/tap"
 tap "heroku/brew"
 tap "homebrew/bundle"
 tap "homebrew/cask"
@@ -8,27 +10,26 @@ tap "homebrew/core"
 tap "homebrew/services"
 tap "neovim/neovim"
 tap "puma/puma"
+tap "sass/sass"
 tap "thoughtbot/formulae"
 tap "tomanthony/brews"
 
 brew "awscli"
-brew "bash-completion"
 brew "bundler-completion"
 brew "dnsmasq"
 brew "editorconfig"
+brew "gh"
 brew "gist"
 brew "git"
 brew "gnu-sed"
 brew "gnu-tar"
-brew "lua"
 brew "htop"
-brew "macvim"
+brew "macvim", link: false
 brew "mas"
 brew "neovim"
 brew "nginx", restart_service: true
-brew "nmap"
-brew "offlineimap"
 brew "openjdk@8"
+brew "pick"
 brew "postgresql", restart_service: true
 brew "pyenv"
 brew "ruby-build"
@@ -42,8 +43,9 @@ brew "terminal-notifier"
 brew "the_silver_searcher"
 brew "tldr"
 brew "tmux"
+brew "trash"
 brew "tree"
-brew "vim", link: false
+brew "vim"
 brew "watchman"
 brew "wget"
 brew "youtube-dl"
@@ -56,13 +58,19 @@ brew "tomanthony/brews/itermocil"
 cask "1password"
 cask "alfred"
 cask "android-studio"
-cask "arq"
+cask "backblaze"
+cask "bartender"
+cask "beekeeper-studio"
 cask "brave-browser"
+cask "carbon-copy-cloner"
+cask "choosy"
+cask "chromedriver"
 cask "discord"
 cask "dropbox"
+cask "fantastical"
+cask "figma"
 cask "firefox"
 cask "firefox-developer-edition"
-
 cask "font-andale-mono"
 cask "font-bitstream-vera"
 cask "font-fira-code"
@@ -76,21 +84,17 @@ cask "font-inconsolata"
 cask "font-inter"
 cask "font-lato"
 cask "font-source-code-pro"
-
 cask "fork"
-cask "choosy"
-cask "beekeeper-studio"
-cask "gitup"
 cask "google-chrome"
 cask "hazel"
-cask "iconjar"
 cask "imageoptim"
-cask "istat-menus"
 cask "iterm2"
 cask "kap"
 cask "keybase"
 cask "keyboard-maestro"
 cask "kindle"
+cask "loom"
+cask "minecraft"
 cask "notion"
 cask "numi"
 cask "obsidian"
@@ -98,17 +102,50 @@ cask "openra"
 cask "phantomjs"
 cask "plex-media-player"
 cask "plex-media-server"
-cask "qlcolorcode"
-cask "qlmarkdown"
-cask "quicklook-csv"
-cask "quicklook-json"
+cask "rectangle"
 cask "rescuetime"
 cask "sketch"
 cask "skype"
 cask "slack"
 cask "steam"
+cask "toggl-track"
 cask "transmission"
+cask "transmit"
 cask "visual-studio-code"
 cask "vlc"
+cask "whatsapp"
 cask "wkhtmltopdf"
+cask "zoom"
 
+mas "1Blocker", id: 1365531024
+mas "Banktivity", id: 1480779512
+mas "Charmstone", id: 1563735522
+mas "DaisyDisk", id: 411643860
+mas "Day One", id: 1055511498
+mas "Deliveries", id: 924726344
+mas "Drafts", id: 1435957248
+mas "Gifwit", id: 871200983
+mas "Harvest", id: 506189836
+mas "Instapaper Save", id: 1481302432
+mas "Keynote", id: 409183694
+mas "MarkDownload - Markdown Web Clipper", id: 1554029832
+mas "Mastonaut", id: 1450757574
+mas "Medis", id: 1579200037
+mas "NextDNS", id: 1464122853
+mas "Noizio", id: 928871589
+mas "Numbers", id: 409203825
+mas "Pages", id: 409201541
+mas "Paprika Recipe Manager", id: 451907568
+mas "Patterns", id: 429449079
+mas "PDFOptim", id: 621276561
+mas "Peek", id: 1554235898
+mas "Pins", id: 1547106997
+mas "Pixelmator Pro", id: 1289583905
+mas "Reeder", id: 1529448980
+mas "Sasquatch", id: 1465346522
+mas "Spark", id: 1176895641
+mas "The Unarchiver", id: 425424353
+mas "Things", id: 904280696
+mas "Timery", id: 1425368544
+mas "Tweetbot", id: 1384080005
+mas "Xcode", id: 497799835
---
+++ bin/set-osx-defaults.sh
@@ -0,0 +1,277 @@
+# Sets reasonable OS X defaults.
+#
+# Or, in other words, set shit how I like in OS X.
+#
+# The original idea (and a couple settings) were grabbed from:
+#   https://github.com/mathiasbynens/dotfiles/blob/master/.osx
+#
+# Run ./set-defaults.sh and you'll be good to go.
+
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.osx` has finished
+#while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null
+
+
+#					System Preferences
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# Restart automatically if the computer freezes
+systemsetup -setrestartfreeze on
+
+# Enable full keyboard access for all controls (e.g. enable Tab in modal dialogs)
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+
+# Enable subpixel font rendering on non-Apple LCDs
+defaults write NSGlobalDomain AppleFontSmoothing -int 2
+
+# Set a blazingly fast keyboard repeat rate
+defaults write NSGlobalDomain KeyRepeat -int 0
+defaults write NSGlobalDomain InitialKeyRepeat -int 10
+
+# Enable auto-correct
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool true
+
+
+#							Safari
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# Hide Safari's bookmark bar.
+# defaults write com.apple.Safari ShowFavoritesBar -bool false
+
+# Set up Safari for development.
+defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
+defaults write com.apple.Safari IncludeDevelopMenu -bool true
+defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
+defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
+defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+
+# Disable Safari’s thumbnail cache for History and Top Sites
+defaults write com.apple.Safari DebugSnapshotsUpdatePolicy -int 2
+
+# Make Safari’s search banners default to Contains instead of Starts With
+defaults write com.apple.Safari FindOnPageMatchesWordStartsOnly -bool false
+
+# Remove useless icons from Safari’s bookmarks bar
+defaults write com.apple.Safari ProxiesInBookmarksBar "()"
+
+
+#							Menu Bar
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Show remaining battery time; hide percentage
+defaults write com.apple.menuextra.battery ShowPercent -string "NO"
+defaults write com.apple.menuextra.battery ShowTime -string "YES"
+
+
+#							Finder
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Always open everything in Finder's list view. This is important.
+defaults write com.apple.Finder FXPreferredViewStyle Nlsv
+
+# Show the ~/Library folder.
+chflags nohidden ~/Library
+
+# Set the Finder prefs for showing a few different volumes on the Desktop.
+defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
+defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
+
+# Save to disk (not to iCloud) by default
+# defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+
+# Use AirDrop over every interface. srsly this should be a default.
+defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
+
+# Expand save panel by default
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+
+# Expand print panel by default
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
+
+# Disable the “Are you sure you want to open this application?” dialog
+defaults write com.apple.LaunchServices LSQuarantine -bool false
+
+# Display ASCII control characters using caret notation in standard text views
+# Try e.g. `cd /tmp; unidecode "\x{0000}" > cc.txt; open -e cc.txt`
+defaults write NSGlobalDomain NSTextShowsControlCharacters -bool true
+
+# Disable opening and closing window animations
+defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false
+
+#Disable window animations and Get Info animations in Finder:
+defaults write com.apple.finder DisableAllAnimations -bool true
+
+# Allow quitting Finder via ⌘ + Q; doing so will also hide desktop icons
+defaults write com.apple.finder QuitMenuItem -bool true
+
+# Disable window animations and Get Info animations in Finder
+defaults write com.apple.finder DisableAllAnimations -bool true
+
+# Allow text selection in Quick Look
+defaults write com.apple.finder QLEnableTextSelection -bool true
+
+# Disable disk image verification
+defaults write com.apple.frameworks.diskimages skip-verify -bool true
+defaults write com.apple.frameworks.diskimages skip-verify-locked -bool true
+defaults write com.apple.frameworks.diskimages skip-verify-remote -bool true
+
+# Automatically open a new Finder window when a volume is mounted
+defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true
+defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
+defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true
+
+# Display full POSIX path as Finder window title
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+
+# Avoid creating .DS_Store files on network volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+
+# Disable the warning when changing a file extension
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+
+#							Mission Control
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# Disable Mission contol animation
+defaults write com.apple.dock expose-animation-duration -float .2
+
+#							Google Chrome
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# Allow installing user scripts via GitHub or Userscripts.org
+defaults write com.google.Chrome ExtensionInstallSources -array "https://*.github.com/*" "http://userscripts.org/*"
+defaults write com.google.Chrome.canary ExtensionInstallSources -array "https://*.github.com/*" "http://userscripts.org/*"
+
+#							Misc apps
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# Automatically quit printer app once the print jobs complete
+defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
+
+# Set Help Viewer windows to non-floating mode
+defaults write com.apple.helpviewer DevMode -bool true
+
+# Disable press-and-hold for keys in favor of key repeat.
+defaults write -g ApplePressAndHoldEnabled -bool false
+
+# Increase window resize speed for Cocoa applications
+defaults write NSGlobalDomain NSWindowResizeTime -float 0.001
+
+# Disable Resume system-wide
+# defaults write NSGlobalDomain NSQuitAlwaysKeepsWindows -bool false
+
+# Enable tap to click (Trackpad) for this user and for the login screen
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
+# Map bottom right Trackpad corner to right-click
+#defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadCornerSecondaryClick -int 2
+#defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadRightClick -bool true
+#defaults -currentHost write NSGlobalDomain com.apple.trackpad.trackpadCornerClickBehavior -int 1
+#defaults -currentHost write NSGlobalDomain com.apple.trackpad.enableSecondaryClick -bool true
+
+# Enable access for assistive devices
+# echo -n 'a' | sudo tee /private/var/db/.AccessibilityAPIEnabled > /dev/null 2>&1
+# sudo chmod 444 /private/var/db/.AccessibilityAPIEnabled
+
+# Require password immediately after sleep or screen saver begins
+#defaults write com.apple.screensaver askForPassword -int 1
+#defaults write com.apple.screensaver askForPasswordDelay -int 0
+
+# Show prompt when power button is pressed on mavericks rather than sleep
+defaults write com.apple.loginwindow PowerButtonSleepsSystem -bool no
+
+# Show item info below desktop icons
+/usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:showItemInfo true" ~/Library/Preferences/com.apple.finder.plist
+
+# Enable snap-to-grid for desktop icons
+/usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+
+# Disable the warning before emptying the Trash
+defaults write com.apple.finder WarnOnEmptyTrash -bool false
+
+# Empty Trash securely by default
+# defaults write com.apple.finder EmptyTrashSecurely -bool true
+
+# Enable highlight hover effect for the grid view of a stack (Dock)
+defaults write com.apple.dock mouse-over-hilte-stack -bool true
+
+# Enable spring loading for all Dock items
+defaults write com.apple.dock enable-spring-load-actions-on-all-items -bool true
+
+# Show indicator lights for open applications in the Dock
+defaults write com.apple.dock show-process-indicators -bool true
+
+# Don’t animate opening applications from the Dock
+defaults write com.apple.dock launchanim -bool false
+
+# Remove the auto-hiding Dock delay
+defaults write com.apple.Dock autohide-delay -float 0
+# Remove the animation when hiding/showing the Dock
+defaults write com.apple.dock autohide-time-modifier -float 0
+
+# Enable the 2D Dock
+defaults write com.apple.dock no-glass -bool true
+
+# Automatically hide and show the Dock
+defaults write com.apple.dock autohide -bool true
+
+# Make Dock icons of hidden applications translucent
+defaults write com.apple.dock showhidden -bool true
+
+# Enable iTunes track notifications in the Dock
+#defaults write com.apple.dock itunes-notifications -bool true
+
+# Add a spacer to the left side of the Dock (where the applications are)
+#defaults write com.apple.dock persistent-apps -array-add '{tile-data={}; tile-type="spacer-tile";}'
+# Add a spacer to the right side of the Dock (where the Trash is)
+#defaults write com.apple.dock persistent-others -array-add '{tile-data={}; tile-type="spacer-tile";}'
+
+# Disable shadow in screenshots
+defaults write com.apple.screencapture disable-shadow -bool false
+
+# Enable the debug menu in Address Book
+defaults write com.apple.addressbook ABShowDebugMenu -bool true
+
+# Disable the debug menu in iCal
+defaults write com.apple.iCal IncludeDebugMenu -bool false
+
+# Only use UTF-8 in Terminal.app
+defaults write com.apple.terminal StringEncodings -array 4
+
+# Enable “focus follows mouse” for Terminal.app and all X11 apps
+# This means you can hover over a window and start typing in it without clicking first
+#defaults write com.apple.terminal FocusFollowsMouse -bool true
+#defaults write org.x.X11 wm_ffm -bool true
+
+# Make ⌘ + F focus the search input in iTunes
+defaults write com.apple.iTunes NSUserKeyEquivalents -dict-add "Target Search Field" "@F"
+
+# Disable send and reply animations in Mail.app
+defaults write com.apple.Mail DisableReplyAnimations -bool true
+defaults write com.apple.Mail DisableSendAnimations -bool true
+
+# Copy email addresses as `foo@example.com` instead of `Foo Bar <foo@example.com>` in Mail.app
+defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false
+
+# Disable the dashboard all together
+defaults write com.apple.dashboard mcx-disabled -bool true
+
+# Reset Launchpad
+[ -e ~/Library/Application\ Support/Dock/*.db ] && rm ~/Library/Application\ Support/Dock/*.db
+
+# Prevent Time Machine from prompting to use new hard drives as backup volume
+defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
+
+# Disable local Time Machine backups
+# hash tmutil &> /dev/null && sudo tmutil disablelocal
+
+# Remove Dropbox’s green checkmark icons in Finder
+#file=/Applications/Dropbox.app/Contents/Resources/check.icns
+#[ -e "$file" ] && mv -f "$file" "$file.bak"
+#unset file
+
+# Kill affected applications
+for app in Finder Dock Mail Safari iTunes iCal Address\ Book SystemUIServer; do killall "$app" > /dev/null 2>&1; done
+echo "Done. Note that some of these changes require a logout/restart to take effect."
